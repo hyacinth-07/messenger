ca1bf7821b0aad525329baf23bdd390c18efd35d
hyacinth-07 <ndr.hya37@gmail.com>
2024-06-24 10:45:41 +0200

    delete updates automatically

--- client/src/utilities/actions.jsx
+++ client/src/utilities/actions.jsx
@@ -1,37 +1,3 @@
-async function postNewThread() {
-	try {
-		const data = await fetch('http://localhost:3000/app/', {
-			method: 'POST',
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({ title: 'Cricket Thread', comments: [] }),
-		});
-		return data;
-	} catch (error) {
-		console.log(error);
-		return null;
-	}
-}
-
-async function deleteThread() {
-	try {
-		const data = await fetch('http://localhost:3000/app/', {
-			method: 'DELETE',
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({ id: '6673f545fe7361fa70f30bcb' }),
-		});
-		return data;
-	} catch (error) {
-		console.log(error);
-		return null;
-	}
-}
-
 export async function threadActions({ request }) {
 	let formData = await request.formData();
 	let intent = formData.get('intent');
@@ -57,7 +23,7 @@ export async function threadActions({ request }) {
 
 	if (intent === 'delete') {
 		try {
-			const data = await fetch('http://localhost:3000/app/', {
+			await fetch('http://localhost:3000/app/', {
 				method: 'DELETE',
 				headers: {
 					Accept: 'application/json',
@@ -65,7 +31,7 @@ export async function threadActions({ request }) {
 				},
 				body: JSON.stringify({ id: deleteId }),
 			});
-			return data;
+			return null;
 		} catch (error) {
 			console.log(error);
 			return null;
--- server/controllers/threadController.js
+++ server/controllers/threadController.js
@@ -1,5 +1,16 @@
 const Thread = require('../models/thread');
 
+// get all threads
+
+exports.getAll = async (req, res) => {
+	try {
+		const threads = await Thread.find();
+		res.status(200).json(threads);
+	} catch (err) {
+		res.status(400).json({ error: err.message });
+	}
+};
+
 // create new thread
 
 exports.create = async (req, res) => {
@@ -15,17 +26,6 @@ exports.create = async (req, res) => {
 	}
 };
 
-// get all threads
-
-exports.getAll = async (req, res) => {
-	try {
-		const threads = await Thread.find();
-		res.status(200).json(threads);
-	} catch (err) {
-		res.status(400).json({ error: err.message });
-	}
-};
-
 // delete thread
 
 exports.delete = async (req, res) => {
@@ -34,7 +34,8 @@ exports.delete = async (req, res) => {
 	const { id } = req.body;
 
 	try {
-		await Thread.findByIdAndDelete(id);
+		const deletedThread = await Thread.findByIdAndDelete(id);
+		res.status(200).json(deletedThread);
 	} catch (err) {
 		res.status(400).json({ error: err.message });
 	}
