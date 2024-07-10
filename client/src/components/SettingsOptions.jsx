d092ae98f37066dc010afe2410f260eaccb4f719
hyacinth-07 <ndr.hya37@gmail.com>
2024-07-10 10:42:06 +0200

    mock settings options

---
+++ client/src/components/SettingsOptions.jsx
@@ -0,0 +1,8 @@
+export default function SettingsOptions() {
+	return (
+		<>
+			<p>Change you username</p>
+			<p>change your password</p>
+		</>
+	);
+}
--- client/src/routes/SettingsPage.jsx
+++ client/src/routes/SettingsPage.jsx
@@ -1,7 +1,24 @@
+import { Link } from 'react-router-dom';
+import SettingsOptions from '../components/SettingsOptions';
+
 export default function SettingsPage() {
 	return (
 		<>
-			<p>A settings page</p>
+			<main className="flex flex-col w-full h-full p-2">
+				<header className="border-2 border-black border-solid min-h-12">
+					<div className="flex justify-evenly h-full items-center">
+						<Link to="/app">
+							<p>Go Back</p>
+						</Link>
+						<Link to="/">
+							<button>Log Out</button>
+						</Link>
+					</div>
+				</header>
+				<section className="flex flex-col h-full mt-2 border-2 border-red-700 border-solid">
+					<SettingsOptions />
+				</section>
+			</main>
 		</>
 	);
 }
